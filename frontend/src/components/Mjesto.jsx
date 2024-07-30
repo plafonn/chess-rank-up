1f7f920d0ddbf270aa19c8c2b2e46114e4828adf
Marija Bilješko <marija.biljesko@fer.hr>
2024-07-30 18:48:45 +0200

    Update Mjesto.jsx

--- frontend/src/components/Mjesto.jsx
+++ frontend/src/components/Mjesto.jsx
@@ -2,19 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./Mjesto.css";
 
 const Mjesto = ({mjesto}) => {
-    const getTopValue = (place) => {
-        return `${7 * (place-4) + 30}vh`; 
-    };
-
-    const ds = {
-        top: getTopValue(mjesto.place),
-    };
 
     return (
         <>
-            <div className='mjesto' style={ds}>
+            <div className='mjesto'>
                 <div className='maleniM1'>{mjesto.place}.</div>
-                <div className='maleniM2'>{mjesto.name}</div>
+                <div className='maleniM2'>{mjesto.firstName} {mjesto.lastName}</div>
                 <div className='maleniM3'>{mjesto.points}</div>
                 </div>
         </>
