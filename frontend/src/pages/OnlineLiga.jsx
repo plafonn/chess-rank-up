9dde56fd463efe4e795b4e116807cc9da7bde211
Marija Bilješko <marija.biljesko@fer.hr>
2024-07-30 18:50:28 +0200

    Add files via upload

---
+++ frontend/src/pages/OnlineLiga.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import TopTri from "../components/TopTri";
+import Mjesto from "../components/Mjesto";
+import './OnlineLiga.css';
+
+const OnlineLiga = () => {
+    const mjesta = [
+        { place: 1, firstName: "Ime", lastName: "Prezime", points: 10 },
+        { place: 2, firstName: "Ime", lastName: "Prezime", points: 8 },
+        { place: 3, firstName: "Ime", lastName: "Prezime", points: 6 }
+    ];
+
+    const topTriMjesta = mjesta.slice(0, 3);
+    const preostalaMjesta = mjesta.slice(3);
+
+    return (
+        <>
+                <div className="up2">
+                    <div className="onlineliga">ONLINE LIGA</div>
+                    <TopTri mjesta={topTriMjesta} />
+                </div>
+
+                <div className="content">
+                    <div className="mjesta">
+                        {preostalaMjesta.map(mjesto => (
+                            <Mjesto mjesto={mjesto} />
+                        ))}
+                    </div>
+                </div>
+        </>
+    );
+}
+
+export default OnlineLiga;
