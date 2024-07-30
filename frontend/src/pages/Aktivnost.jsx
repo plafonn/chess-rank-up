f785bf25c09a0c96c143330032b53cc5204fa217
Marija Bilješko <marija.biljesko@fer.hr>
2024-07-30 18:41:49 +0200

    Aktivnost.jsx

--- frontend/src/pages/Aktivnost.jsx
+++ frontend/src/pages/Aktivnost.jsx
@@ -1,34 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import'./Aktivnost.css';
+import './Aktivnost.css';
 import Event from '../components/Event';
 import nazad from '../images/nazad.png';
 
-
 const Aktivnost = () => {
-    const testEvent1 = {
-        id: 1,
-        date: "2024-07-25",
-        name: "Test Event1",
-        points: 2
+    const initialEvents = [
+        {
+            id: 1,
+            date: "2024-07-25",
+            name: "Test Event1",
+            defaultPoints: 2
+        },
+        {
+            id: 2,
+            date: "2024-10-26",
+            name: "Test Event2",
+            defaultPoints: 1
+        },
+        {
+            id: 3,
+            date: "2023-01-11",
+            name: "Test Event3",
+            defaultPoints: 1
+        }
+    ];
+
+    const novi = {
+        id: 4,
+        date: "2024-08-24",
+        name: "NOVI",
+        defaultPoints: 1
     };
-    const testEvent2 = {
-        id: 2,
-        date: "2024-10-26",
-        name: "Test Event2",
-        points: 1
+
+    const sortEvents = (events) => {
+        return [...events].sort((a, b) => {
+            const dateA = new Date(a.date);
+            const dateB = new Date(b.date);
+            return dateB - dateA; 
+        });
     };
-    const testEvent3 = {
-        id: 3,
-        date: "2023-01-11",
-        name: "Test Event3",
-        points: 1
-    };
+
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        setEvents(sortEvents(initialEvents));
+    }, []);
+
+    useEffect(() => {
+        setEvents(prevEvents => sortEvents([...prevEvents, novi]));
+    }, []);
 
     return (
-       <>  
-       <div className='bodycontainer'>
-            <Link to="/profil"><img src={nazad} className="nazad" alt="nazad"/></Link>
+        <div className='bodycontainer'>
+            <Link to="/profil"><img src={nazad} className="nazad" alt="nazad" /></Link>
             
             <div className='bold'>Ime Prezime</div>
 
@@ -38,13 +63,12 @@ const Aktivnost = () => {
                 <div className='maleni3'>Bodovi</div>
             </div>
 
-            <Event event={testEvent1} />
-            <Event event={testEvent2} />
-            <Event event={testEvent3} />
-       </div> 
-       
-       
-       </>
+            <div className='eventi'>
+                {events.map(event => (
+                    <Event event={event} />
+                ))}
+            </div>
+        </div>
     );
 };
 
